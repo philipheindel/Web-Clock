d27a71c2a55d486f0c0634c9306c39ba57ddb0b9
Scott Fox <foxs02@students.ipfw.edu>
2018-12-02 22:52:04 +0000

    removing the 12/24 hr buttons

--- views/index/indexBody.php
+++ views/index/indexBody.php
@@ -17,20 +17,7 @@
                 <div id="Clock" class="clock">00:00:00</div>
             </div>
         </div>
-        <div class="row">
-            <div class="col menu-bar main-content">
-                <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                    <label onclick = "resetTime(); showTheTime24('-5,0')" class="btn btn-sm btn-outline-primary" onmouseout="$('#twentyfourHour').blur()">
-                        <input  id="twentyfourHour" type="radio" name="options" autocomplete="off">
-                        24 Hr.
-                    </label>
-                    <label onclick = "resetTime(); showTheTime('-5,0')"class="btn btn-sm btn-outline-primary active" onmouseout="$('#twelveHour').blur()">
-                        <input id="twelveHour" type="radio" name="options" autocomplete="off">
-                        12 Hr.
-                    </label>
-                </div>
-            </div>
-        </div>
+        
         <div class="row">
             <div class="col menu-bar main-content">
                 <div class="btn-group btn-group-toggle" data-toggle="buttons">
