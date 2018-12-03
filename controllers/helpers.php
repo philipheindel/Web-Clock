485c151a47f8000c26b0badab1d6be12d75f8585
Philip Heindel <p.e.heindel@gmail.com>
2018-12-03 04:51:04 -0500

    Formatted helpers.php code.

--- controllers/helpers.php
+++ controllers/helpers.php
@@ -18,50 +18,53 @@ function render($template, $data = array())
         require $path;
     }
 }
-function connection($servername, $username, $password){
-$conn = new mysqli($servername, $username, $password, $dbname);
+function connection($servername, $username, $password)
+{
+    $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    echo "<br>";
-} 
-    
-return $conn;
-}
-
-function connectionDB($servername, $username, $password, $dbname){
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    echo "<br>";
-} 
-
-    
-return $conn;
-}
-
-function createDB($conn, $dbname){
-    
-// Create database
-$sql = "CREATE DATABASE $dbname";
-if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
-    echo "<br>";
-} else {
-    echo "Error creating database: " . $conn->error;
-    echo "<br>";
-}    
-    
-}
-
-function createTable($conn, $dbname, $dbtable){
     if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+        die("Connection failed: " . $conn->connect_error);
+        echo "<br>";
+    }
+
+    return $conn;
+}
+
+function connectionDB($servername, $username, $password, $dbname)
+{
+    $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        echo "<br>";
+    }
+
+    return $conn;
+}
+
+function createDB($conn, $dbname)
+{
+
+// Create database
+    $sql = "CREATE DATABASE $dbname";
+    if ($conn->query($sql) === true) {
+        echo "Database created successfully";
+        echo "<br>";
+    } else {
+        echo "Error creating database: " . $conn->error;
+        echo "<br>";
+    }
+
+}
+
+function createTable($conn, $dbname, $dbtable)
+{
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
 // sql to create table
-$sql = "CREATE TABLE $dbtable (
+    $sql = "CREATE TABLE $dbtable (
 fName VARCHAR(30) NOT NULL,
 lName VARCHAR(30) NOT NULL,
 email VARCHAR(30) NOT NULL,
@@ -69,91 +72,86 @@ password VARCHAR(30) NOT NULL,
 verifyPassword VARCHAR(30) NOT NULL
 )";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Table $dbtable created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
+    if ($conn->query($sql) === true) {
+        echo "Table $dbtable created successfully";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
 }
 
-function addToTable($conn, $dbtable,$fname, $lname,$email,$password,$verifyPassword){
-//function used for adding info into the table    
-    
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+function addToTable($conn, $dbtable, $fname, $lname, $email, $password, $verifyPassword)
+{
+//function used for adding info into the table
 
-$sql = "INSERT INTO $dbtable (fName, lName, email, password, verifyPassword)
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO $dbtable (fName, lName, email, password, verifyPassword)
 VALUES ('$fname','$lname','$email','$password','$verifyPassword')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Account created!";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-    
+    if ($conn->query($sql) === true) {
+        echo "Account created!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
 }
 
-function updateRow($conn, $postBackcolor, $postHeadercolor, $secSwitch, $apSwitch, $sound, $emailNotif, $browserNotif , $userid)
+function updateRow($conn, $postBackcolor, $postHeadercolor, $secSwitch, $apSwitch, $sound, $emailNotif, $browserNotif, $userid)
 {
-    //function used for adding info into the table    
-    
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+    //function used for adding info into the table
 
-$sql = "UPDATE `temp`.`settings` SET `backgroundcol` =  '$postBackcolor',
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "UPDATE `temp`.`settings` SET `backgroundcol` =  '$postBackcolor',
         `headercol` =  '$postHeadercolor', `showsec` = '$secSwitch', `showap` = '$apSwitch',
         `sound` = '$sound', `emailnotif` = '$emailNotif', `browsernotif` '$browserNotif'
             WHERE `settings`.`id` = '$userid';";
 
-if ($conn->query($sql) === TRUE) {
-    echo "updated!";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+    if ($conn->query($sql) === true) {
+        echo "updated!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
 
-function compare($conn, $compareemail, $dbtable){
-    
+function compare($conn, $compareemail, $dbtable)
+{
+
     if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-    
-    
-   
- $sql = "SELECT * FROM `$dbtable` WHERE email='$compareemail'";   
+        die("Connection failed: " . $conn->connect_error);
+    }
 
- $var = $conn->query($sql);
- $result = $var->fetch_assoc();
- 
+    $sql = "SELECT * FROM `$dbtable` WHERE email='$compareemail'";
+
+    $var = $conn->query($sql);
+    $result = $var->fetch_assoc();
+
     return $result[email];
-    
-    
+
 }
 
-function loginChecker($conn,$dbtable,$login,$password){
-   
- 
-$sql = "SELECT * FROM `$dbtable` WHERE email='$login'";
- $var = $conn->query($sql);
- $result = $var->fetch_assoc();
+function loginChecker($conn, $dbtable, $login, $password)
+{
 
- 
- $sql1 = "SELECT * FROM `$dbtable` WHERE email='$login'";
- $var1 = $conn->query($sql1);
- $result1 = $var1->fetch_assoc();
- 
- 
-$email= $result[email];  
-$pass= $result1[password];
+    $sql = "SELECT * FROM `$dbtable` WHERE email='$login'";
+    $var = $conn->query($sql);
+    $result = $var->fetch_assoc();
 
-if($email==$login && $pass == $password){
-    return true;
-}
-else {
-    return false;
-}
-    
-    
+    $sql1 = "SELECT * FROM `$dbtable` WHERE email='$login'";
+    $var1 = $conn->query($sql1);
+    $result1 = $var1->fetch_assoc();
+
+    $email = $result[email];
+    $pass = $result1[password];
+
+    if ($email == $login && $pass == $password) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
