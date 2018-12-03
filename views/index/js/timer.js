44b08e309f99c3b48c7d2fec5def48aa8c63b938
Austin Osborne <osboaj02@students.ipfw.edu>
2018-12-03 05:50:21 +0000

    Change of alert. Takes in a 0 for empty string, and completes rather than alert.

--- views/index/js/timer.js
+++ views/index/js/timer.js
@@ -3,14 +3,17 @@ function startTimer()
 {
     
     var h = parseInt(document.getElementById('timerHours').value);
+    h = isNaN(h) ? 0 : h;
     var m = parseInt(document.getElementById('timerMinutes').value);
+    m = isNaN(m) ? 0 : m;
     var s = parseInt(document.getElementById('timerSeconds').value);
+    s = isNaN(s) ? 0 : s;
    
-    if(isNaN(h) || isNaN(m) || isNaN(s))
-    {
-        alert("You must input numbers!");
-        return false;
-    }
+    // if(isNaN(h) || isNaN(m) || isNaN(s))
+    // {
+    //     alert("You must input numbers!");
+    //     return false;
+    // }
     
     current = ((h*3600) + (m*60) + s);
     timer = setInterval(setTimer,1000);
@@ -22,7 +25,7 @@ function stopTimer()
 
 function setTimer()
 {
-
+    
     if(current == 0)
     {    
         clearInterval(timer);
@@ -35,13 +38,9 @@ function setTimer()
         var minutes = Math.floor((current/60)%60);
         var seconds = Math.floor(current % 60);
 
-        
         document.getElementById("output").innerHTML = hours +"h " + minutes + "m " + seconds + " s";
         
         current--;
-
     }
-    
-   
-    }
+}
    
