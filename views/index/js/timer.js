43f106e2724485a24b98f6dd0957ba8d875ec579
Austin Osborne <osboaj02@students.ipfw.edu>
2018-12-03 03:14:58 +0000

    Got timer functionality working. Now working on making it look better in HH:MM:SS format

--- views/index/js/timer.js
+++ views/index/js/timer.js
@@ -1,50 +1,34 @@
+var timer, current;
+function startTimer()
+{
+    
+    var h = document.getElementById('timerHours').value;
+    var m = document.getElementById('timerMinutes').value;
+    var s = document.getElementById('timerSeconds').value;
+    
+    current = parseInt((h*2160) + (m*60) + s);
+    timer = setInterval(setTimer,1000);
+}
+function stopTimer()
+{
+    clearInterval(timer);
+}
 
-//Commenting that out..starting my own method for the countdown
-var timer;
 function setTimer()
 {
-    var h = parseInt(document.getElementById("timerHours").value);
-    var m = parseInt(document.getElementById("timerMinutes").value);
-    var s = parseInt(document.getElementById("timerSeconds").value);
-    
-    var current = (h*2160) + (m*60) + s;
-    /*
-    clearInterval(timer);
-    //Set variables to take in the hours, minute, and second user inputs 
-    //Also set a display time, of the inputted values.
-    var h = parseInt(document.getElementById("timerHours").value);
-    var m = parseInt(document.getElementById("timerMinutes").value);
-    var s = parseInt(document.getElementById("timerSeconds").value);
 
-    var timer_date = h + " : " + m + " : " +  s;
-    var now = new Date();
-    now.setHours(h);
-    now.setMinutes(m);
-    now.setSeconds(s);
-    //An inner function to determine what to output...then outputs it.
-    function showTimer()
+    
+    
+    if(current == 0)
     {
-        var now = new Date();
-        
-        var remain = end - now;
-        if(remain < 0)
-        {
-            clearInterval(timer);
-            document.getElementById("timer_value").innerHTML = 'Reached!'; 
-            return;
-        }
-     //Get the remaining hours
-        var hours = Math.floor(remain / minute);
-        var minutes = Math.floor((remain % hour)/minute);   //Get the remainint minutes
-        var seconds = Math.floor((remain % minute)/second); //Get the remaining seconds
-        
-        //Print statements to output the time left.
-       document.getElementById('timer_value').innerHTML = hours + 'Hours';
-        document.getElementById('timer_value').innerHTML += minutes + 'Minutes ';
-        document.getElementById('timer_value').innerHTML += seconds + 'Seconds ';
-        */
-        
+        clearInterval(timer);
     }
-    timer = setInterval(showTimer, 1000);//Display the timer in every one second.
-  
-}
+    else
+    {
+        current--;
+        document.getElementById("timer_value").innerHTML = current;
+    }
+    
+   
+    }
+   
